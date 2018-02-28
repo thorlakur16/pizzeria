32d5d2ca99e4dc0ab605107d449af92e8906be91
Þorlákur Lyngmo <thorlakur16@ru.is>
2018-02-28 08:43:29 +0000

    aðeins meira dót

--- src/components/Pizzas/Pizzas.js
+++ src/components/Pizzas/Pizzas.js
@@ -2,18 +2,34 @@ import React from 'react';
 
 class Pizzas extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.updateData = this.updateData.bind(this);
+        this.state = {
+            data: {},
+            dataArray: []
+        };
     }
 
     componentDidMount() {
-        fetch('http://localhost:3500/api/pizzas').then(response => response.json()).then(json => console.log(json));
+        fetch('http://localhost:3500/api/pizzas').then(response => response.json()).then(json =>{this.updateData(json)});
+    }
+
+    updateData(data) {
+        this.setState({data: data});
     }
 
     render() {
+        const { data } = this.state;
+        console.log(data);
+        for (var o in data) {
+            this.dataArray.push(o);
+        }
+        console.log(this.dataArray);
         return (
             <div>
                 Pizzas
+
             </div>
         )
     };
