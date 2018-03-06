0a9dbce75b1a025ef7b83cffee6e00a515975848
Þorlákur Lyngmo <thorlakur16@ru.is>
2018-03-06 17:36:29 +0000

    meira details. save to localStorage

--- src/components/PizzaDetails/PizzaDetails.js
+++ src/components/PizzaDetails/PizzaDetails.js
@@ -3,40 +3,43 @@ import React from 'react';
 class PizzaDetails extends React.Component {
 
     componentDidMount() {
+        if(this.props.children){
+            this.setState({selectedPizza: this.props.currentPizza});
+        }
+
         var order = JSON.parse(localStorage.getItem('order'));
         console.log(order);
-        this.setState({selectedPizza: this.props.currentPizza});
+
     }
 
     constructor(props) {
         super(props);
+        this.addToOrder = this.addToOrder.bind(this);
         this.state = {
             order: [],
-            selectedPizza: {}
-        }
-    }
-
-    addToOrder() {
-        var order = JSON.parse(localStorage.getItem('order'));
-        console.log(order);
-        let thingToAdd = this.state.selectedPizza;
-        console.log(thingToAdd);
-        localStorage.setItem('order', JSON.stringify(thingToAdd));
-    }
-
-    render() {
-
-
-        if(this.state.selectedPizza === undefined) {
-            let _selectedPizza = {
+            selectedPizza: {
                 id: 69,
                 name: "Cheesus Crust",
                 description: "Double cheese on double cheese. All the cheese",
                 price: 6000,
                 image: "http://snworksceo.imgix.net/ohi/ead6a57c-1980-4033-80cd-4d9a9aa9c197.sized-1000x1000.jpg"
-            };
-            this.setState({selectedPizza: _selectedPizza});
+            }
         }
+    }
+
+    addToOrder() {
+        let order = JSON.parse(localStorage.getItem('order'));
+        let arr = [];
+        arr.push(this.state.selectedPizza);
+        for(let i = 0; i < order.length; i++){
+            arr.push(order[i]);
+        }
+        console.log(arr);
+
+        localStorage.setItem('order', JSON.stringify(arr));
+    }
+
+    render() {
 
         const { selectedPizza } = this.state;
 
@@ -44,14 +47,14 @@ class PizzaDetails extends React.Component {
 
         return (
             <div>
-                <h2>{_selectedPizza.name}</h2>
+                <h2>{selectedPizza.name}</h2>
                 <div>
-                    <img src={_selectedPizza.image} alt={_selectedPizza.name} />
+                    <img src={selectedPizza.image} alt={selectedPizza.name} />
                 </div>
                 <p>
-                    {_selectedPizza.description}
+                    {selectedPizza.description}
                 </p>
-                <div>Price: {_selectedPizza.price} </div>
+                <div>Price: {selectedPizza.price} </div>
                 <button id="addToCart" onClick={this.addToOrder}>Add to cart</button>
             </div>
         )
