9f42373a7abaf24d3c24d7caffcf77efcab50594
unknown <binnib5@gmail.com>
2018-03-08 13:08:48 +0000

    subit form fiff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
 import './App.css';
-import Pizzas from "./components/Pizzas/Pizzas";
+import Pizzas from './components/Pizzas/Pizzas';
 
-import Offers from "./components/Offers/Offers";
-//import Drasl from "./components/Drasl/Drasl";
-import NavBar from "./components/NavBar/NavBar";
+import Offers from './components/Offers/Offers';
+//import Drasl from './components/Drasl/Drasl';
+import NavBar from './components/NavBar/NavBar';
 import MuiThemeProvder from 'material-ui/styles/MuiThemeProvider';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import About from './components/About/About';
 import Cart from './components/Cart/Cart';
-import PizzaDetails from "./components/PizzaDetails/PizzaDetails";
+import PizzaDetails from './components/PizzaDetails/PizzaDetails';
+import Form from './components/Form/Form';
 
 class App extends Component {
 
@@ -35,17 +36,15 @@ class App extends Component {
                     <NavBar/>
                     <div className='main-container'>
                         <Switch>
-                            <Route path='/:pizzaId' component={PizzaDetails} currentPizza={this.state.currentPizza}/>
                             <Route path='/about' component={About}/>
                             <Route path='/offers' component={Offers}/>
                             <Route path='/cart' component={Cart}/>
+                            <Route path='/submit' component={Form}/>
                             <Route exact path='/menu' render={() => {
                                 return <Redirect to='/'/>
                             }}/>
                             <Route exact path='/' component={Pizzas} changeCurrentPizza={this.changeCurrentPizza}/>
-
-
-
+                            <Route path='/:pizzaId' component={PizzaDetails} currentPizza={this.state.currentPizza}/>
                         </Switch>
                     </div>
                 </div>
