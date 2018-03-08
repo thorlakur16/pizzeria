4594422c2ba5aa1848812a3bb346d5692b38266a
thorlakur16 <thorlakur16@ru.is>
2018-03-08 23:20:32 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
 
     componentDidMount() {
         //this.props.getPizzaPrice();
-        localStorage.clear();
+        //localStorage.clear();
     }
 
     render() {
