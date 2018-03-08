9ae4ded46e2563debed2669b314c43062af6dbbd
Þorlákur Lyngmo <thorlakur16@ru.is>
2018-03-08 16:11:04 +0000

    getAllPizzas

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,30 @@
 import priceService from "../services/priceService";
+import pizzaService from "../services/pizzaService";
+import fetch from 'isomorphic-fetch';
 
 export const getDetails = (data) => {
     return {
         type: 'DETAILS',
         payload: data
     }
+};
+
+export const getAllPizzas = () => {
+    return (dispatch) => {
+        return pizzaService.getPizzas().then(data => {
+            console.log('í getAllPizzas' +data);
+            dispatch(getAllPizzaSuccess(data));
+
+        });
+    }
+};
+
+const getAllPizzaSuccess = (pizzas) => {
+    console.log(pizzas);
+    return {
+        type: 'GET_ALL_PIZZAS',
+        payload: pizzas
+    };
 };
 
 export const getPizzaPrice = () => {
